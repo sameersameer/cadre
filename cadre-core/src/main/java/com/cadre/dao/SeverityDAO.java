55d22855470df9f9ded0b6bbbee3d8f0366f4f0c
Purushotham233 <purushotham.m@flipkart.com>
2016-09-11 03:20:09 +0530

    Aggregator Job

--- cadre-core/src/main/java/com/cadre/dao/SeverityDAO.java
+++ cadre-core/src/main/java/com/cadre/dao/SeverityDAO.java
@@ -1,8 +1,11 @@
 package com.cadre.dao;
 
+import org.joda.time.DateTime;
+import org.skife.jdbi.v2.sqlobject.Bind;
 import org.skife.jdbi.v2.sqlobject.SqlQuery;
 
 import java.math.BigDecimal;
+import java.sql.Timestamp;
 import java.util.List;
 import java.util.Map;
 
@@ -13,11 +16,23 @@ public interface SeverityDAO {
     static final String tableName = "severity";
 
     @SqlQuery("select device_id, sum(distance) from "+tableName+" where time1 > UNIX_TIMESTAMP(CURDATE()) group by 1;")
-    Map.Entry<String,BigDecimal> getDailyDistance();
+    Map<String,BigDecimal> getDailyDistance();
 
     @SqlQuery("select device_id, sum(distance) from "+tableName+" where time1 > UNIX_TIMESTAMP(DATE_ADD(CURDATE(),INTERVAL -(DAYOFWEEK(CURRDATE()))+1 DAY)) group by 1;")
-    Map.Entry<String,BigDecimal> getWeeklyDistance();
+    Map<String,BigDecimal> getWeeklyDistance();
 
     @SqlQuery("select device_id, sum(distance) from "+tableName+" group by 1;")
-    Map.Entry<String,BigDecimal> getTotalDistance();
+    Map<String,BigDecimal> getTotalDistance();
+
+    @SqlQuery("select device_id, sum(distance)*severity*:price from "+tableName+" where time1 > UNIX_TIMESTAMP(CURDATE()) group by 1;")
+    Map<String,BigDecimal> getDailyEarnings(@Bind("price")double price);
+
+    @SqlQuery("select device_id, sum(distance)*severity*:price from "+tableName+" where time1 > UNIX_TIMESTAMP(DATE_ADD(CURDATE(),INTERVAL -(DAYOFWEEK(CURRDATE()))+1 DAY)) group by 1;")
+    Map<String,BigDecimal> getWeeklyEarning(@Bind("price")double price);
+
+    @SqlQuery("select device_id, sum(distance)*severity*:price from "+tableName+" group by 1;")
+    Map<String,BigDecimal> getTotalEarning(@Bind("price")double price);
+
+    @SqlQuery("select UNIX_TIMESTAMP(max(last_updated_at)) from "+tableName)
+    Long getLastUpdated();
 }
