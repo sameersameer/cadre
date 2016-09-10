811093a8c69bd7f981741a692485c54e8c0a6e4a
Purushotham233 <purushotham.m@flipkart.com>
2016-09-11 02:17:36 +0530

    Stats and severity dao

--- cadre-core/src/main/java/com/cadre/dao/SeverityDAO.java
+++ cadre-core/src/main/java/com/cadre/dao/SeverityDAO.java
@@ -1,6 +1,10 @@
 package com.cadre.dao;
 
+import org.skife.jdbi.v2.sqlobject.SqlQuery;
+
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by purushotham.m on 10/09/16.
@@ -8,4 +12,12 @@ import java.util.List;
 public interface SeverityDAO {
     static final String tableName = "severity";
 
+    @SqlQuery("select device_id, sum(distance) from "+tableName+" where time1 > UNIX_TIMESTAMP(CURDATE()) group by 1;")
+    Map.Entry<String,BigDecimal> getDailyDistance();
+
+    @SqlQuery("select device_id, sum(distance) from "+tableName+" where time1 > UNIX_TIMESTAMP(DATE_ADD(CURDATE(),INTERVAL -(DAYOFWEEK(CURRDATE()))+1 DAY)) group by 1;")
+    Map.Entry<String,BigDecimal> getWeeklyDistance();
+
+    @SqlQuery("select device_id, sum(distance) from "+tableName+" group by 1;")
+    Map.Entry<String,BigDecimal> getTotalDistance();
 }
--- cadre-core/src/main/java/com/cadre/dao/StatsDAO.java
+++ cadre-core/src/main/java/com/cadre/dao/StatsDAO.java
@@ -3,6 +3,8 @@ package com.cadre.dao;
 import com.cadre.entities.Creative;
 import com.cadre.entities.Stats;
 import org.skife.jdbi.v2.sqlobject.Bind;
+import org.skife.jdbi.v2.sqlobject.BindBean;
+import org.skife.jdbi.v2.sqlobject.SqlBatch;
 import org.skife.jdbi.v2.sqlobject.SqlQuery;
 import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapper;
 
@@ -12,6 +14,19 @@ import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapper;
 @RegisterMapper(Stats.StatsMapper.class)
 public interface StatsDAO {
     static final String tableName = "stats";
-    @SqlQuery("SELECT * FROM " + tableName + " where id = :id")
+    @SqlQuery("SELECT * FROM " + tableName + " where device_idd = :id")
     Stats getDeviceStats(@Bind("id") String deviceId);
+
+    @SqlBatch("INSERT INTO `"+tableName+"` (`device_id`, `mileage_d`, `mileage_w`, `mileage_t`, `earnings_d`, `earnings_w`, `earnings_t`, `last_udpated_at`)\n" +
+            "VALUES\n" +
+            "(:deviceStat.deviceId, :deviceStat.dayMileage, :deviceStat.weekMileage, :deviceStat.totalMileage, :deviceStat.dayEarning, :deviceStat.weekEarning, :deviceStat.totalEarning, :deviceStat.lastUpdatedAt)\n" +
+            "ON DUPLICATE KEY UPDATE \n" +
+            "  mileage_d = VALUES(mileage_d),\n" +
+            "  mileage_w = VALUES(mileage_w),\n" +
+            "  mileage_t = VALUES(mileage_t),\n" +
+            "  earnings_d = VALUES(earning_d),\n" +
+            "  earnings_w = VALUES(earning_w),\n" +
+            "  earnings_t = VALUES(earning_t),\n" +
+            "  last_updated_at = VALUES(last_updated_at);")
+    int insertOrUpdate(@BindBean("deviceStat") Iterable<Stats> deviceStat);
 }
