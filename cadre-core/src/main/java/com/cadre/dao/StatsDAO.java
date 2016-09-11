2bd1a2bdf24ffbe623cbc95803b7cf93c9b5da7b
Purushotham233 <purushotham.m@flipkart.com>
2016-09-11 11:54:01 +0530

    Fixed Aggregator Job

--- cadre-core/src/main/java/com/cadre/dao/StatsDAO.java
+++ cadre-core/src/main/java/com/cadre/dao/StatsDAO.java
@@ -6,7 +6,10 @@ import org.skife.jdbi.v2.sqlobject.Bind;
 import org.skife.jdbi.v2.sqlobject.BindBean;
 import org.skife.jdbi.v2.sqlobject.SqlBatch;
 import org.skife.jdbi.v2.sqlobject.SqlQuery;
+import org.skife.jdbi.v2.sqlobject.SqlUpdate;
 import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapper;
+
+import java.util.List;
 
 /**
  * Created by purushotham.m on 10/09/16.
@@ -14,19 +17,18 @@ import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapper;
 @RegisterMapper(Stats.StatsMapper.class)
 public interface StatsDAO {
     static final String tableName = "stats";
+
     @SqlQuery("SELECT * FROM " + tableName + " where device_idd = :id")
     Stats getDeviceStats(@Bind("id") String deviceId);
 
-    @SqlBatch("INSERT INTO `"+tableName+"` (`device_id`, `mileage_d`, `mileage_w`, `mileage_t`, `earnings_d`, `earnings_w`, `earnings_t`, `last_udpated_at`)\n" +
-            "VALUES\n" +
-            "(:deviceStat.deviceId, :deviceStat.dayMileage, :deviceStat.weekMileage, :deviceStat.totalMileage, :deviceStat.dayEarning, :deviceStat.weekEarning, :deviceStat.totalEarning, :deviceStat.lastUpdatedAt)\n" +
+    @SqlUpdate("INSERT INTO `"+tableName+"` (device_id, mileage_d, mileage_w, mileage_t, earnings_d, earnings_w, earnings_t)\n" +
+            "VALUES (:deviceStat.deviceId, :deviceStat.dayMileage, :deviceStat.weekMileage, :deviceStat.totalMileage, :deviceStat.dayEarning, :deviceStat.weekEarning, :deviceStat.totalEarning)\n"+
             "ON DUPLICATE KEY UPDATE \n" +
-            "  mileage_d = VALUES(mileage_d),\n" +
-            "  mileage_w = VALUES(mileage_w),\n" +
-            "  mileage_t = VALUES(mileage_t),\n" +
-            "  earnings_d = VALUES(earning_d),\n" +
-            "  earnings_w = VALUES(earning_w),\n" +
-            "  earnings_t = VALUES(earning_t),\n" +
-            "  last_updated_at = VALUES(last_updated_at);")
-    int insertOrUpdate(@BindBean("deviceStat") Iterable<Stats> deviceStat);
+                        "  mileage_d = VALUES(mileage_d),\n" +
+                        "  mileage_w = VALUES(mileage_w),\n" +
+                        "  mileage_t = VALUES(mileage_t),\n" +
+                        "  earnings_d = VALUES(earnings_d),\n" +
+                        "  earnings_w = VALUES(earnings_w),\n" +
+                        "  earnings_t = VALUES(earnings_t);")
+    void insertOrUpdate(@BindBean("deviceStat") Stats deviceStat);
 }
